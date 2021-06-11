e3939857b666f8022949aa560a5f709862d9f28f
Richard Robinson <richard@rr.codes>
2021-06-11 13:28:09 -0400

    handles editing an issue

--- src/action.ts
+++ src/action.ts
@@ -3,14 +3,25 @@ import * as core from '@actions/core';
 import type {IssuesEditedEvent, IssuesOpenedEvent} from '@octokit/webhooks-definitions/schema';
 import type {WebhookPayload} from '@actions/github/lib/interfaces';
 import {properties} from './properties';
+import {InputPropertyValueMap} from '@notionhq/client/build/src/api-endpoints';
 
-interface Options {
-  notion: {
-    token: string;
-    databaseId: string;
-  };
-  github: {
-    payload: WebhookPayload;
+function parsePropertiesFromPayload(
+  payload: IssuesOpenedEvent | IssuesEditedEvent
+): InputPropertyValueMap {
+  return {
+    Name: properties.title(payload.issue.title),
+    Organization: properties.richText(payload.organization?.login ?? ''),
+    Repository: properties.richText(payload.repository.name),
+    Number: properties.number(payload.issue.number),
+    Body: properties.richText(payload.issue.body),
+    Assignees: properties.richText(payload.issue.assignees.map(user => user.login).join(', ')),
+    Milestone: properties.richText(payload.issue.milestone?.title ?? ''),
+    Labels: properties.richText(payload.issue.labels?.map(label => label.name).join(', ') ?? ''),
+    Author: properties.richText(payload.issue.user.login),
+    Created: properties.date(payload.issue.created_at),
+    Updated: properties.date(payload.issue.updated_at),
+    Status: properties.richText(payload.issue.state ?? ''),
+    ID: properties.number(payload.issue.id),
   };
 }
 
@@ -31,20 +42,7 @@ async function handleIssueOpened(options: IssueOpenedOptions) {
     parent: {
       database_id: notion.databaseId,
     },
-    properties: {
-      Name: properties.title(payload.issue.title),
-      Organization: properties.richText(payload.organization?.login ?? ''),
-      Repository: properties.richText(payload.repository.name),
-      Number: properties.number(payload.issue.number),
-      Body: properties.richText(payload.issue.body),
-      Assignees: properties.richText(payload.issue.assignees.map(user => user.login).join(', ')),
-      Milestone: properties.richText(payload.issue.milestone?.title ?? ''),
-      Labels: properties.richText(payload.issue.labels?.map(label => label.name).join(', ') ?? ''),
-      Author: properties.richText(payload.issue.user.login),
-      Created: properties.date(payload.issue.created_at),
-      Updated: properties.date(payload.issue.updated_at),
-      Status: properties.richText(payload.issue.state),
-    },
+    properties: parsePropertiesFromPayload(payload),
   });
 }
 
@@ -56,9 +54,47 @@ interface IssueEditedOptions {
   payload: IssuesEditedEvent;
 }
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-// @ts-expect-error implement
-async function handleIssueEdited(options: IssueEditedOptions) {}
+async function handleIssueEdited(options: IssueEditedOptions) {
+  const {notion, payload} = options;
+
+  core.info(`Querying database for page with github id ${payload.issue.id}`);
+
+  const query = await notion.client.databases.query({
+    database_id: notion.databaseId,
+    filter: {
+      property: 'ID',
+      number: {
+        equals: payload.issue.id,
+      },
+    },
+    page_size: 1,
+  });
+
+  if (query.results.length === 0) {
+    core.warning(`Could not find page with github id ${payload.issue.id}`);
+    return;
+  }
+
+  const pageId = query.results[0].id;
+
+  core.info(`Query successful: Page ${pageId}`);
+  core.info(`Updating page for issue #${payload.issue.number}`);
+
+  await notion.client.pages.update({
+    page_id: pageId,
+    properties: parsePropertiesFromPayload(payload),
+  });
+}
+
+interface Options {
+  notion: {
+    token: string;
+    databaseId: string;
+  };
+  github: {
+    payload: WebhookPayload;
+  };
+}
 
 export async function run(options: Options) {
   const {notion, github} = options;
