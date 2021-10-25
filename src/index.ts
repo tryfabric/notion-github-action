ec5f7d24c14b879bbf73373606a9c1f3539abcd7
Julia Rosenson <juliarosenson@Julias-MBP.lan>
2021-10-25 11:53:17 -0400

    Revert "echo context"
    This reverts commit ea9949a77350190f212871a1dd7569e7b4e1c14a.

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,6 @@ async function start() {
     core.info(`context event: ${github.context.eventName}`);
     core.info(`context action: ${github.context.action}`);
     core.info(`payload action: ${github.context.payload.action}`);
-    core.info(`context: ${github.context}`);
 
     const options = {
       notion: {
