4608251c911c1eb87c0e10662e6d70235d440e3a
Julia Rosenson <juliarosenson@Julias-MBP.lan>
2021-10-25 11:52:51 -0400

    Revert "log context payload repo"
    This reverts commit 6167c798a3928f829e2b532304eaabe1ec54052f.

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ async function start() {
     core.info(`context event: ${github.context.eventName}`);
     core.info(`context action: ${github.context.action}`);
     core.info(`payload action: ${github.context.payload.action}`);
-    core.info(`context payload repo: ${github.context.payload.repostiory}`);
+    core.info(`context: ${github.context}`);
 
     const options = {
       notion: {
